a0e0f78edd54473569c43c5cd5b4a6f130a54e89
ysdxz207 <ysdxz207@qq.com>
2018-09-30 23:27:42 +0800

    js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import './assets/icon/iconfont.css'
 import Global from '~/global/Global'
