659be58ad748e89f8a230f36b697220b205febe5
Moses <ysdxz207@qq.com>
2018-10-11 18:34:10 +0800

    logging page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,77 +1,63 @@
 <template>
-    <el-container>
-        <el-header></el-header>
-        <el-main>
-            <el-row :gutter="20">
-                <el-col :span="16" :xs="0" :sm="14" class="login-left">&nbsp;</el-col>
-                <el-col :span="8" :xs="24" :sm="10" class="login-right">
-                    <el-form ref="user" :model="user" :rules="rules" label-width="80px">
-                        <el-form-item>
-                            <span class="login-right-title">登录管理后台</span>
-                        </el-form-item>
-                        <el-form-item label="用户名" prop="username">
-                            <el-input v-model="user.username"
-                                      @keyup.enter.native="login">
-                                <i slot="prefix" class="element-icons el-icon-user"></i>
-                            </el-input>
-                        </el-form-item>
-                        <el-form-item label="密码" prop="password">
-                            <el-input v-model="user.password" type="password"
-                                      @keyup.enter.native="login">
-                                <i slot="prefix" class="element-icons el-icon-password"></i>
-                            </el-input>
-                        </el-form-item>
-                        <el-form-item label="验证码" prop="captcha">
-                            <el-row>
-                                <el-col :span="18">
-                                    <el-input v-model="user.captcha"
-                                              @keyup.enter.native="login">
-                                        <i slot="prefix" class="element-icons el-icon-user"></i>
-                                    </el-input>
-                                </el-col>
-                                <el-col :span="6">
-                                    <img :src="captchaImg" @click="getCaptcha" style="cursor: pointer"/>
-                                </el-col>
-                            </el-row>
+    <div>
+        <el-container>
+            <canvas id="canvas"></canvas>
+            <el-header></el-header>
+            <el-main>
+                        <el-form ref="user" :model="user" :rules="rules" label-width="80px">
+                            <el-form-item>
+                                <span class="login-right-title">登录管理后台</span>
+                            </el-form-item>
+                            <el-form-item label="用户名" prop="username">
+                                <el-input v-model="user.username"
+                                          @keyup.enter.native="login" autocomplete="off">
+                                    <i slot="prefix" class="element-icons el-icon-user"></i>
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item label="密码" prop="password">
+                                <el-input v-model="user.password" type="password"
+                                          @keyup.enter.native="login" autocomplete="off">
+                                    <i slot="prefix" class="element-icons el-icon-password"></i>
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item label="验证码" prop="captcha">
+                                <el-row>
+                                    <el-col :span="18">
+                                        <el-input v-model="user.captcha"
+                                                  @keyup.enter.native="login" autocomplete="off">
+                                            <i slot="prefix" class="element-icons el-icon-user"></i>
+                                        </el-input>
+                                    </el-col>
+                                    <el-col :span="6">
+                                        <img :src="captchaImg" @click="getCaptcha" style="cursor: pointer"/>
+                                    </el-col>
+                                </el-row>
 
-                        </el-form-item>
-                        <el-form-item prop="rememberMe"
-                                      class="login-remember-me-group">
-                            <el-checkbox v-model="user.rememberMe"
-                                         class="login-remember-me-checkbox">自动登录</el-checkbox>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" class="btn-login" :loading="loading" @click="login">登录</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-col>
-            </el-row>
-        </el-main>
-    </el-container>
+                            </el-form-item>
+                            <el-form-item prop="rememberMe"
+                                          class="login-remember-me-group">
+                                <el-checkbox v-model="user.rememberMe"
+                                             class="login-remember-me-checkbox">自动登录
+                                </el-checkbox>
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button type="primary" class="btn-login" :loading="loading" @click="login">登录
+                                </el-button>
+                            </el-form-item>
+                        </el-form>
+            </el-main>
+        </el-container>
+
+    </div>
 </template>
 
 <script>
     import request from '../utils/request'
-    import Bubbles from './canvas/bubbles/index'
 
     export default {
-        components: {
-            Bubbles
-        },
-        data () {
+        components: {},
+        data() {
             return {
-                bubbleOptions: {
-                    len: 40, //The unilateral length of the pentagon
-                    count: 50, //How many lines overlap
-                    rate: 20, //rate the smaller fast
-                    dieChance: 0.05, //The chance to redraw a single painting failure.
-                    sparkChance: 0.1, //[0,1] The larger the figure, the larger the pentagon.
-                    sparkDist: 10, //Flash point distance.
-                    sparkSize: 2,//Flash point size.
-                    contentLight: 60, // [0,100] Brightness of color block
-                    shadowToTimePropMult: 6, //The shadow size of the inner ring of the pentagon.
-                    bgColorArr: [0, 0, 0] //Background color array
-                },
                 user: {
                     username: '',
                     password: '',
@@ -81,16 +67,16 @@
                 captchaImg: '',
                 rules: {
                     username: [
-                        { required: true, message: '请输入用户名', trigger: 'blur' },
-                        { min: 1, max: 64, message: '长度在 1 到 64 个字符', trigger: 'blur' }
+                        {required: true, message: '请输入用户名', trigger: 'blur'},
+                        {min: 1, max: 64, message: '长度在 1 到 64 个字符', trigger: 'blur'}
                     ],
                     password: [
-                        { required: true, message: '请输入密码', trigger: 'blur' },
-                        { min: 1, max: 64, message: '长度在 1 到 64 个字符', trigger: 'blur' }
+                        {required: true, message: '请输入密码', trigger: 'blur'},
+                        {min: 1, max: 64, message: '长度在 1 到 64 个字符', trigger: 'blur'}
                     ],
                     captcha: [
-                        { required: true, message: '请输入验证码', trigger: 'blur' },
-                        { min: 4, max: 4, message: '长度为4个字符', trigger: 'blur' }
+                        {required: true, message: '请输入验证码', trigger: 'blur'},
+                        {min: 4, max: 4, message: '长度为4个字符', trigger: 'blur'}
                     ]
                 },
                 loading: false
@@ -98,6 +84,7 @@
         },
         mounted() {
             this.getCaptcha()
+            this.setCanvas()
         },
         methods: {
             getCaptcha() {
@@ -155,34 +142,163 @@
                     })
                 })
 
+            },
+            setCanvas() {
+                let canvas = document.getElementById('canvas'),
+                    ctx = canvas.getContext('2d'),
+                    w = canvas.width = window.innerWidth,
+                    h = canvas.height = window.innerHeight,
+
+                    hue = 217,
+                    stars = [],
+                    count = 0,
+                    maxStars = 1300//星星数量
+
+                let canvas2 = document.createElement('canvas'),
+                    ctx2 = canvas2.getContext('2d')
+                canvas2.width = 100
+                canvas2.height = 100
+                let half = canvas2.width / 2,
+                    gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half)
+                gradient2.addColorStop(0.025, '#CCC')
+                gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)')
+                gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)')
+                gradient2.addColorStop(1, 'transparent')
+
+                ctx2.fillStyle = gradient2
+                ctx2.beginPath()
+                ctx2.arc(half, half, half, 0, Math.PI * 2)
+                ctx2.fill()
+
+                function random(min, max) {
+                    if (arguments.length < 2) {
+                        max = min
+                        min = 0
+                    }
+
+                    if (min > max) {
+                        let hold = max
+                        max = min
+                        min = hold
+                    }
+
+                    return Math.floor(Math.random() * (max - min + 1)) + min
+                }
+
+                function maxOrbit(x, y) {
+                    let max = Math.max(x, y),
+                        diameter = Math.round(Math.sqrt(max * max + max * max))
+                    //星星移动范围，值越大范围越小，
+                    return diameter / 2
+                }
+
+                let Star = function () {
+
+                    this.orbitRadius = random(maxOrbit(w, h))
+                    this.radius = random(60, this.orbitRadius) / 8
+                    //星星大小
+                    this.orbitX = w / 2
+                    this.orbitY = h / 2
+                    this.timePassed = random(0, maxStars)
+                    //星星移动速度
+                    this.speed = random(this.orbitRadius) / 60000;
+                    this.alpha = random(2, 10) / 10
+
+                    count++
+                    stars[count] = this
+                }
+
+                Star.prototype.draw = function () {
+                    let x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+                        y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+                        twinkle = random(10)
+
+                    if (twinkle === 1 && this.alpha > 0) {
+                        this.alpha -= 0.05
+                    } else if (twinkle === 2 && this.alpha < 1) {
+                        this.alpha += 0.05
+                    }
+
+                    ctx.globalAlpha = this.alpha
+                    ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+                    this.timePassed += this.speed
+                }
+
+                for (let i = 0; i < maxStars; i++) {
+                    new Star()
+                }
+
+                function animation() {
+                    ctx.globalCompositeOperation = 'source-over'
+                    ctx.globalAlpha = 0.5 //尾巴
+                    ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 2)'
+                    ctx.fillRect(0, 0, w, h)
+
+                    ctx.globalCompositeOperation = 'lighter'
+                    for (let i = 1, l = stars.length; i < l; i++) {
+                        stars[i].draw()
+                    }
+
+                    window.requestAnimationFrame(animation)
+                }
+
+                animation()
             }
         },
-        created () {
+        created() {
         },
-        watch: {
-        }
+        watch: {}
     }
 </script>
 
 <style lang="less" scoped>
     .login-right-title {
-        color: #5c5c5c;
+        color: #DDD;
         font-size: 3vh;
         font-weight: bold;
     }
+
     .btn-login {
         width: 100%;
     }
+
     .login-remember-me-group {
         margin-bottom: 8px;
     }
+
     .login-remember-me-checkbox {
         float: left;
     }
 
+    canvas {
+        width: 100%;
+        height: 100%;
+        display: inline-block;
+        vertical-align: baseline;
+        position: absolute;
+        z-index: 0;
+    }
 
     .el-container {
+        position: relative;
         height: 100vh;
+        width: 100%;
+        /*background-color: #000;*/
         background: linear-gradient(to top left, #41a37b, #51dda2);
+    }
+
+    .el-form {
+        width: 420px;
+        margin: 0 auto;
+    }
+
+    /deep/ .el-input input {
+        color: #DDD;
+        border-radius: 0;
+        border-top: 0;
+        border-left: 0;
+        border-right: 0;
+        border-bottom: 1px solid #DDD;
+        background-color:transparent;
     }
 </style>
